88afce9c4ebc70649cf5fba173803b171f8cf720
Gi <71566094+Gipria@users.noreply.github.com>
2020-12-10 12:04:42 -0300

    adjust all

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,15 +1,32 @@
-//Arryay []
-const map = L.map('mapid').setView([-22.9196882,-47.1066941,12.75], 17)
+//create map
+const map = L.map('mapid').setView([-27.222633,-49.6455874], 15)
 
 //create and add titleLayer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }
-).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+.addTo(map);
 
 
+//create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58,68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+})
+
+
+//create popup overlay
+const popup = L.popup({
+  closeButton: false,
+  className: 'map-popup',
+  minWidth: 240,
+  minHeight:240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+
+
+//create and ass marker
 L
-.marker([-22.9196882,-47.1066941,12.75]).addTo(map)
-.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-.openPopup();
+.marker([-27.222633,-49.6455874], {icon})
+.addTo(map)
+.bindPopup(popup)
+
